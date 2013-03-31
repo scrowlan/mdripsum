323c0d57a262bb4d014859e0f66187a1834b4605
Scott Rowland <scrowlan@gmail.com>
2013-03-31 10:08:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdripsum::Application.config.secret_token = '5a0b069e76f46a615a03560ec88fb6cd456a5bfca09c1a4d07c50238ef3a8bf080d4e62e69f900feb4d8942446ffa66101f730ea844210fd845f1b2b9a30c68d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mdripsum::Application.config.session_store :cookie_store, key: '_mdripsum_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mdripsum::Application.config.session_store :active_record_store
